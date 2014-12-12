bdcdaad54704638b6cee1ce6baea19aa2662e79b
HappySecondBrother <xinyoulingxi2008@126.com>
2014-12-12 10:02:48 +0800

    web config

---
+++ secbro-web/src/main/java/org/secbroframework/web/MyFreeMarkerView.java
@@ -0,0 +1,17 @@
+package org.secbroframework.web;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
+
+public class MyFreeMarkerView extends FreeMarkerView {
+    private static final String CONTEXT_PATH = "base";
+
+    @Override
+    protected void exposeHelpers(Map<String, Object> model, HttpServletRequest request) throws Exception {
+        model.put(CONTEXT_PATH, request.getContextPath());
+        super.exposeHelpers(model, request);
+    }
+}
